af2b72c628ae4c517c49ba58491effa9210d533a
Hjollsari <hjorleifursveins@gmail.com>
2025-03-20 20:29:30 +0000

    Question-2

---
+++ src/main/java/is/hi/hbv202g/ass9/composite/PlusComposite.java
@@ -0,0 +1,20 @@
+package is.hi.hbv202g.ass9.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlusComposite implements MathExpression {
+    private List<MathExpression> children = new ArrayList<>();
+
+    public void add(MathExpression mathExpression) {
+        children.add(mathExpression);
+    }
+
+    public int getResult() {
+        int result = 0;
+        for (MathExpression child : children) {
+            result += child.getResult();
+        }
+        return result;
+    }
+}
